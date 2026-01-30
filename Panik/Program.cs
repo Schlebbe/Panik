f6e1174d25198c29d6ec07c035f368c29efae724
sebastian@temtron.se <sebastian@temtron.se>
2026-01-30 09:23:03 +0100

    Add project files.

---
+++ Panik/Program.cs
@@ -0,0 +1,40 @@
+
+using Scalar.AspNetCore;
+
+namespace Panik
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.MapScalarApiReference();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapGet("/", (HttpContext httpContext) =>
+            {
+                
+                return null;
+            });
+
+            app.Run();
+        }
+    }
+}
